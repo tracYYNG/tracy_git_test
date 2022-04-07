2b0b56497b794872a3c241202d8ded82033e17d3
tracY <582974285@qq.com>
2022-04-07 19:04:02 +0800

    模型创建

--- mysite/test_01/models.py
+++ mysite/test_01/models.py
@@ -1,3 +1,21 @@
+from operator import mod
+from statistics import mode
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+class BookInfo(models.Model):
+
+    name = models.CharField(max_length=20,verbose_name='Book Name')
+    pub_date = models.DateField(verbose_name='Release Time',null=True)
+    readcount = models.IntegerField(default=0,verbose_name='Reading Volume')
+    commentcount = models.IntegerField(default=0,verbose_name='Comment Volume')
+    id_delete = models.BooleanField(default=False,verbose_name='Logical Deletion')
+
+    class Meta:
+        db_table = 'bookinfo'
+        verbose_name = 'book'
+
+
+    def __str__(self) -> str:
+        return self.name
